a31ab0bed2bd1639aecc60ef7f26059db11bc337
SBTUR Principal <sbturviagens@Mac-mini-de-SBTUR.local>
2015-08-31 13:45:00 -0300

    Initial Commit

---
+++ cidades/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cidades
+//
+//  Created by SBTUR Principal on 8/31/15.
+//  Copyright (c) 2015 Ariane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
